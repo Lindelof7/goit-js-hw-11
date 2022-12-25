02404375af4b6e22aa19e8932be3a4cedbe8b046
Volodymyr Butyter <butytervolodia@gmail.com>
2022-12-25 19:02:16 +0200

    90

--- src/index.js
+++ src/index.js
@@ -1,24 +1,58 @@
 import './css/styles.css'
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { fetchPhotos } from './fetch-photos'
 
 const inputEl = document.querySelector('input[name = "searchQuery"]');
 const submitBtnEL = document.querySelector('.submit');
 const loadMoreBtnEl = document.querySelector('.load-more')
+const galleryEl = document.querySelector('.gallery')
 
-console.log(loadMoreBtnEl)
-const addCard = `<div class="photo-card">
-    <img src="" alt="" loading="lazy" />
+
+
+console.log(inputEl)
+
+inputEl.addEventListener('input', onSearch)
+
+
+async function onSearch(e) {
+    e.preventDefault();
+    const searchQuery = e.target.value.trim();
+    console.log(searchQuery)
+    if (searchQuery !== "") {
+        try {
+            const result = await fetchPhotos(searchQuery)
+            console.log(result)
+            const photos = await createPhoto(result.hits)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+}
+
+async function createPhoto(photos) {
+    const createdPhotos = photos.map((photo) => {
+        const mappedPhotos = `<div class="photo-card">
+    <img src="${photo.webformatURL}" width = "380px" alt="${photo.tags}" loading="lazy" />
     <div class="info">
-        <p class="info-item">
-            <b>Likes</b>    
+        <p class="info-item">   
+            <b>Likes ${photo.likes}</b>    
         </p>
         <p class="info-item">
-            <b>Views</b>
+            <b>Views ${photo.views}</b>
         </p>
         <p class="info-item">
-            <b>Comments</b>
+            <b>Comments ${photo.comments}</b>
         </p>
         <p class="info-item">
-            <b>Downloads</b>
+            <b>Downloads ${photo.downloads}</b>
         </p>
     </div>
-</div>`;
+</div>`
+        console.log(mappedPhotos)
+        return mappedPhotos;
+    }).join('')
+    galleryEl.innerHTML = createdPhotos
+}
