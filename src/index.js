42173a630c95af190e999f98e08b2882a8dfd5ba
Danyil <danyildiabin@ukr.net>
2022-12-25 23:16:45 +0200

    style fixes

--- src/index.js
+++ src/index.js
@@ -1,44 +1,35 @@
-import './css/styles.css'
+import './css/styles.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import simpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { fetchPhotos } from './fetch-photos'
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchPhotos } from './fetch-photos.js';
 
 const inputEl = document.querySelector('input[name = "searchQuery"]');
 const submitBtnEL = document.querySelector('.submit');
-const loadMoreBtnEl = document.querySelector('.load-more')
-const galleryEl = document.querySelector('.gallery')
+const loadMoreBtnEl = document.querySelector('.load-more');
+const galleryEl = document.querySelector('.gallery');
 
-
-
-console.log(inputEl)
-
-inputEl.addEventListener('input', onSearch)
-
-
-async function onSearch(e) {
-    e.preventDefault();
-    const searchQuery = e.target.value.trim();
-    console.log(searchQuery)
-    if (searchQuery !== "") {
+inputEl.addEventListener('input', async (event) => {
+    event.preventDefault();
+    const searchQuery = event.target.value.trim();
+    if (searchQuery !== '') {
         try {
-            const result = await fetchPhotos(searchQuery)
-            console.log(result)
-            const photos = await createPhoto(result.hits)
+            const result = await fetchPhotos(searchQuery);
+            spawnPhotos(result.hits);
         } catch (error) {
-            console.log(error.message)
+            console.error(error.message);
         }
     }
-}
+});
 
-async function createPhoto(photos) {
-    const createdPhotos = photos.map((photo) => {
-        const mappedPhotos = `<div class="photo-card">
-    <img src="${photo.webformatURL}" width = "400px"  alt="${photo.tags}" loading="lazy" />
+function spawnPhotos(photos) {
+    galleryEl.innerHTML = photos.map(photo => {
+            return `<div class="photo-card">
+    <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
     <div class="info">
-        <p class="info-item">   
-            <b>Likes ${photo.likes}</b>    
+        <p class="info-item">
+            <b>Likes ${photo.likes}</b>
         </p>
         <p class="info-item">
             <b>Views ${photo.views}</b>
@@ -50,9 +41,6 @@ async function createPhoto(photos) {
             <b>Downloads ${photo.downloads}</b>
         </p>
     </div>
-</div>`
-        console.log(mappedPhotos)
-        return mappedPhotos;
-    }).join('')
-    galleryEl.innerHTML = createdPhotos
+</div>`;
+    }).join('');
 }
