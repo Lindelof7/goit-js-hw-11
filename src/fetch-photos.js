6f5d9b8e8b7945ebbf1bb0e1d498af52d8d18f1d
Volodymyr Butyter <butytervolodia@gmail.com>
2022-12-28 13:58:58 +0200

    styles

--- src/fetch-photos.js
+++ src/fetch-photos.js
@@ -3,14 +3,6 @@ export async function fetchPhotos(searchQuery, page) {
     return response.json();
 
 }
-// export async function fetchPhotos(searchQuery) {
-//     const response = await axios.get(`https://pixabay.com/api/?key=32358654-06404774fd2fdef00d453a3c4&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&`);
-//     console.log(response)
-//     const data = await response.data.json();
-// }
-
-
-
 
 // return fetch(`https://pixabay.com/api/?key=32358654-06404774fd2fdef00d453a3c4&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
 //     .catch((err) => console.error(err.message))
