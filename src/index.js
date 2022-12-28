b35f475ed8ea68bc0e305a340ec49c06dafcbe7e
Volodymyr Butyter <butytervolodia@gmail.com>
2022-12-28 14:06:29 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -87,16 +87,16 @@ function addPhotos(photos) {
     </a>
     <div class="info">
         <p class="info-item">
-            <b>Likes: ${photo.likes}</b>
+            <b>Likes ${photo.likes}</b>
         </p>
         <p class="info-item">
-            <b>Views: ${photo.views}</b>
+            <b>Views ${photo.views}</b>
         </p>
         <p class="info-item">
-            <b>Comments: ${photo.comments}</b>
+            <b>Comments ${photo.comments}</b>
         </p>
         <p class="info-item">
-            <b>Downloads: ${photo.downloads}</b>
+            <b>Downloads ${photo.downloads}</b>
         </p>
     </div>
 </div>`;
